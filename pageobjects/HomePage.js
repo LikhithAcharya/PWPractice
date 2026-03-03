beac22bb70655e7be26ffcc65531eadc7a7c1536
Likhith <likhithacharya277@gmail.com>
2026-03-03 22:39:27 +0530

    try 2

--- pageobjects/HomePage.js
+++ pageobjects/HomePage.js
@@ -15,7 +15,7 @@ exports.HomePage = class HomePage {
     {
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
-        await console.log("Login button is visible:");
+      //  await console.log("Login button is visible:");
         await this.loginButton.click();
     }
     
